26b15fbd1aff974a7d5862d33aead0f12977ac02
Samuel Hinkhouse <hinkhouse.samuel@gmail.com>
2024-10-27 23:39:23 -0500

    bug: fix issue where modal would not load each time

--- chorus-connector/src/app/pages/choruses/choruses.module.ts
+++ chorus-connector/src/app/pages/choruses/choruses.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ChorusesPageRoutingModule } from './choruses-routing.module';
 
 import { ChorusesPage } from './choruses.page';
+import { ComponentsModule } from 'src/app/core/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ChorusesPageRoutingModule
+    ChorusesPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [ChorusesPage]
 })
--- chorus-connector/src/app/pages/choruses/choruses.page.ts
+++ chorus-connector/src/app/pages/choruses/choruses.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewChild } from '@angular/core';
-import { IonModal, ModalController, ViewDidEnter, ViewDidLeave } from '@ionic/angular';
+import { IonModal, ModalController, ViewDidEnter, ViewDidLeave, ViewWillEnter } from '@ionic/angular';
 import { ChorusesListComponent } from 'src/app/core/components/choruses-list/choruses-list.component';
 import { ChorusData } from 'src/app/core/mock/choruses';
 import { Choruses } from 'src/app/core/models/chorus';
@@ -11,19 +11,19 @@ import { ChorusesService } from 'src/app/core/services/choruses.service';
   templateUrl: './choruses.page.html',
   styleUrls: ['./choruses.page.scss'],
 })
-export class ChorusesPage implements ViewDidEnter, ViewDidLeave {
+export class ChorusesPage implements ViewDidLeave, ViewWillEnter {
   public searchTerm: string = '';
   private choruses: Choruses = this.choirService.getChoruses();
-  private modal: IonModal | null = null;
+  private modal: HTMLIonModalElement | null = null;
   constructor(private modalController: ModalController, private choirService: ChorusesService) {
     
   }
   ionViewDidLeave(): void {
-    this.modal?.dismiss();
+    this.modalController.dismiss();
+    this.modal!.dismiss();
   }
-  ionViewDidEnter(): void {
-    console.log('enter');
-    this.modalController
+  async ionViewWillEnter() {
+    this.modal = await this.modalController
       .create({
         component: ChorusesListComponent,
         showBackdrop: false,
@@ -35,16 +35,10 @@ export class ChorusesPage implements ViewDidEnter, ViewDidLeave {
         backdropDismiss: false,
         canDismiss: true,
         handle: true,
-      })
-      .then((modal) => {
-        const ionContent = document.querySelector('ion-content');
-        const activeTabPage = ionContent
-          ? ionContent.closest('.ion-page')
-          : null;
-        activeTabPage?.appendChild(modal);
-        this.modal = modal as unknown as IonModal;
-        this.modal.present();
       });
+      const activeTabPage = document.querySelector('#choruses-view')?.closest('.ion-page');
+      activeTabPage?.appendChild(this.modal);
+        this.modal.present();
   }
 
   searchChoruses() {
